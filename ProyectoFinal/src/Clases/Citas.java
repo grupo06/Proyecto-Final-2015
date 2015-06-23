075a3d8392580199aebef2c1c7d7181664cdec3a
WilberparionaHuaraca <deivisspariona@gmail.com>
2015-06-23 08:06:36 -0500

    GRUPO E  AUMENTADO CODIGO EN REFISTRO DE CITAS

--- ProyectoFinal/src/Clases/Citas.java
+++ ProyectoFinal/src/Clases/Citas.java
@@ -7,8 +7,70 @@ package Clases;
 
 /**
  *
- * @author yefers
+ * @author wilber
  */
 public class Citas {
+    
+   private String  Nombre;
+   private String  Apelllido;
+   private String  Telefono;
+   private String  Correo;
+
+    /**
+     * @return the Nombre
+     */
+    public String getNombre() {
+        return Nombre;
+    }
+
+    /**
+     * @param Nombre the Nombre to set
+     */
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    /**
+     * @return the Apelllido
+     */
+    public String getApelllido() {
+        return Apelllido;
+    }
+
+    /**
+     * @param Apelllido the Apelllido to set
+     */
+    public void setApelllido(String Apelllido) {
+        this.Apelllido = Apelllido;
+    }
+
+    /**
+     * @return the Telefono
+     */
+    public String getTelefono() {
+        return Telefono;
+    }
+
+    /**
+     * @param Telefono the Telefono to set
+     */
+    public void setTelefono(String Telefono) {
+        this.Telefono = Telefono;
+    }
+
+    /**
+     * @return the Correo
+     */
+    public String getCorreo() {
+        return Correo;
+    }
+
+    /**
+     * @param Correo the Correo to set
+     */
+    public void setCorreo(String Correo) {
+        this.Correo = Correo;
+    }
+ 
     
 }
--- ProyectoFinal/src/Clases/PersistenciaPersona.java
+++ ProyectoFinal/src/Clases/PersistenciaPersona.java
@@ -15,7 +15,7 @@ import java.util.ArrayList;
 
 /**
  *
- * @author yefers
+ * @author wlber
  */
 public class PersistenciaPersona implements Serializable{
     
